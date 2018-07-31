d859786056c8083523604326efefbb07e331984f
shunmian <shunmian@gmail.com>
2018-08-01 07:55:41 +0800

    add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -282,6 +282,27 @@ describe('POST /users/login', () => {
       })
     })
   })
+})
 
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    const user = newUsers[0]
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', user.tokens[0].token)
+    .send()
+    .expect(200)
+    .end((err, res) => {
+      if(err) {
+        return done(err)
+      }
+      User.findById(newUsers[0]._id).then(user=>{
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch(e=>{
+        done(e)
+      })
+    })
+  })
 })
 
